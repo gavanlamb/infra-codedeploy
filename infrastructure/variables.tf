a79477a14caa81fd1416933d444cb112ce445454
Gavan Lamb <gavan.jon.lamb@gmail.com>
2022-06-25 19:21:26 +1000

    Initial commit

---
+++ infrastructure/variables.tf
@@ -0,0 +1,21 @@
+﻿variable "environment" {
+  type = string
+  description = "Name of the environment the infrastructure is for."
+}
+variable "region" {
+  type = string
+  description = "Name of the AWS region to deploy resources to"
+}
+
+variable "code_deploy_role_name" {
+  type = string
+  description = "Name of the role for CodeDeploy"
+}
+variable "code_deploy_policy_name" {
+  type = string
+  description = "Name of the policy for CodeDeploy"
+}
+variable "code_deploy_bucket_name" {
+  type = string
+  description = "Bucket name for CodeDeploy artifacts."
+}
---
+++ infrastructure/variables/production.ap-southeast-2.tfvars
@@ -0,0 +1,6 @@
+﻿environment="Production"
+region="ap-southeast-2"
+
+code_deploy_role_name="expensely-code-deploy"
+code_deploy_policy_name="expensely-code-deploy"
+code_deploy_bucket_name="expensely-code-deploy-production"
