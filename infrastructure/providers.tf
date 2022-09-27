54735efff3914dc31d5b73f131628d2f3626d3e9
Gavan Lamb <gavan.jon.lamb@gmail.com>
2022-09-27 14:18:58 +1000

    Fix role arn for assume role

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -46,7 +46,7 @@ provider "aws" {
   region  = var.region
 
   assume_role {
-    role_arn = "arn:aws:iam::04633789203:role/terraform.infrastructure"
+    role_arn = "arn:aws:iam::104633789203:role/terraform.infrastructure"
   }
   default_tags {
     tags = {
